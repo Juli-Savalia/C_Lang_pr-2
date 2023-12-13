7ebe0d825020570670d81e1e8bff9fffed39bb2b
Juli <3382julisavalia@gmail.com>
2023-12-13 15:01:34 +0530

    add code

---
+++ Q1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(){
+	
+	int a,b,c;
+	printf("enter the number:- ");
+	scanf("%d",&a);
+	
+	(a%2==0)?printf("The number is Even...\n")
+			  :printf("The number is Odd...");
+}
